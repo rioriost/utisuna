f53a8022bd126f0a8356b566c025d21f8686270d
Rio Fujita <rio_github@rio.st>
2026-09-12 15:30:59 +0900

    Update macOS 27 distribution build and package checksums

--- Formula/utisuna.rb
+++ Formula/utisuna.rb
@@ -1,9 +1,9 @@
 class Utisuna < Formula
   desc 'Set the default app for the content type of a sample file'
   homepage 'https://github.com/rioriost/utisuna'
-  url 'https://github.com/rioriost/utisuna/releases/download/0.1.0/utisuna-0.1.0-macos.zip'
-  sha256 '835418f20da549c20961a18e6fa6172ed8bebd8ccfe79c975d3df4dc367a7f7d'
-  version '0.1.0'
+  url 'https://github.com/rioriost/utisuna/releases/download/0.1.1/utisuna-0.1.1-macos.zip'
+  sha256 '60d7b5132d845d26ade72218130f842cd3e7cef29c800c5685d80f0506120d37'
+  version '0.1.1'
 
   def install
     bin.install 'utisuna' => 'utisuna'
